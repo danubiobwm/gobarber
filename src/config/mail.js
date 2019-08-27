cb8e86d06a23ff8c43458ca87f7f8ee995663d67
danubiobwm <danubio.bwm@gmail.com>
2019-08-26 23:32:05 -0300

    Configurando Nodemailer

---
+++ src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+  host: 'smtp.mailtrap.io',
+  port: 2525,
+  secure: false,
+  auth: {
+    user: '15d048049d6918',
+    pass: 'efaa581426d137',
+  },
+  default: {
+    from: 'Equipe Gobarber <noreply@gobarber.com>',
+  },
+};
